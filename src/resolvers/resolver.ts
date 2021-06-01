97ce7a67d71eba60cd72d20657c128cd865f5e8f
sato.naoya <sato.naoya@classmethod.jp>
2021-06-01 18:01:39 +0900

    inputとenum

--- src/resolvers/resolver.ts
+++ src/resolvers/resolver.ts
@@ -1,15 +1,27 @@
-import { Resolvers } from '../types/generate';
+import { Resolvers, Photo } from '../types/generate';
 
-const photos = [];
+const photos: Array<Photo> = [];
+let id = 0;
 
 export const resolvers: Resolvers = {
     Query: {
         totalPhotos: (): number => photos.length,
+        allPhotos: (): Array<Photo> => photos,
     },
     Mutation: {
-        postPhoto: (parent, args) => {
-            photos.push(args);
-            return true;
+        postPhoto: (parent, args): Photo => {
+            const newPhoto: Photo = {
+                id: (id++).toString(),
+                ...args.input,
+                url: '',
+            };
+
+            photos.push(newPhoto);
+
+            return newPhoto;
         },
+    },
+    Photo: {
+        url: (parent) => `http://youtesite.com/img/${parent.id}.jpg`,
     },
 };
--- src/types/generate.ts
+++ src/types/generate.ts
@@ -1,38 +1,55 @@
 import { GraphQLResolveInfo } from 'graphql';
 export type Maybe<T> = T | null;
-export type Exact<T extends { [key: string]: unknown }> = {
-    [K in keyof T]: T[K];
-};
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> &
-    { [SubKey in K]?: Maybe<T[SubKey]> };
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> &
-    { [SubKey in K]: Maybe<T[SubKey]> };
-export type RequireFields<T, K extends keyof T> = {
-    [X in Exclude<keyof T, K>]?: T[X];
-} &
-    { [P in K]-?: NonNullable<T[P]> };
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type RequireFields<T, K extends keyof T> = { [X in Exclude<keyof T, K>]?: T[X] } & { [P in K]-?: NonNullable<T[P]> };
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-    ID: string;
-    String: string;
-    Boolean: boolean;
-    Int: number;
-    Float: number;
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
 };
 
 export type Mutation = {
-    __typename?: 'Mutation';
-    postPhoto: Scalars['Boolean'];
+  __typename?: 'Mutation';
+  postPhoto: Photo;
 };
 
+
 export type MutationPostPhotoArgs = {
-    name: Scalars['String'];
-    description?: Maybe<Scalars['String']>;
+  input: PostPhotoInput;
+};
+
+export type Photo = {
+  __typename?: 'Photo';
+  id: Scalars['ID'];
+  url: Scalars['String'];
+  name: Scalars['String'];
+  description?: Maybe<Scalars['String']>;
+  category?: Maybe<PhotoCategory>;
+};
+
+export enum PhotoCategory {
+  Selfie = 'SELFIE',
+  Portrait = 'PORTRAIT',
+  Action = 'ACTION',
+  Landscape = 'LANDSCAPE',
+  Graphic = 'GRAPHIC'
+}
+
+export type PostPhotoInput = {
+  name: Scalars['String'];
+  category?: Maybe<PhotoCategory>;
+  description?: Maybe<Scalars['String']>;
 };
 
 export type Query = {
-    __typename?: 'Query';
-    totalPhotos?: Maybe<Scalars['Int']>;
+  __typename?: 'Query';
+  totalPhotos?: Maybe<Scalars['Int']>;
+  allPhotos: Array<Photo>;
 };
 
 export type WithIndex<TObject> = TObject & Record<string, any>;
@@ -40,163 +57,127 @@ export type ResolversObject<TObject> = WithIndex<TObject>;
 
 export type ResolverTypeWrapper<T> = Promise<T> | T;
 
+
 export type LegacyStitchingResolver<TResult, TParent, TContext, TArgs> = {
-    fragment: string;
-    resolve: ResolverFn<TResult, TParent, TContext, TArgs>;
+  fragment: string;
+  resolve: ResolverFn<TResult, TParent, TContext, TArgs>;
 };
 
 export type NewStitchingResolver<TResult, TParent, TContext, TArgs> = {
-    selectionSet: string;
-    resolve: ResolverFn<TResult, TParent, TContext, TArgs>;
+  selectionSet: string;
+  resolve: ResolverFn<TResult, TParent, TContext, TArgs>;
 };
-export type StitchingResolver<TResult, TParent, TContext, TArgs> =
-    | LegacyStitchingResolver<TResult, TParent, TContext, TArgs>
-    | NewStitchingResolver<TResult, TParent, TContext, TArgs>;
+export type StitchingResolver<TResult, TParent, TContext, TArgs> = LegacyStitchingResolver<TResult, TParent, TContext, TArgs> | NewStitchingResolver<TResult, TParent, TContext, TArgs>;
 export type Resolver<TResult, TParent = {}, TContext = {}, TArgs = {}> =
-    | ResolverFn<TResult, TParent, TContext, TArgs>
-    | StitchingResolver<TResult, TParent, TContext, TArgs>;
+  | ResolverFn<TResult, TParent, TContext, TArgs>
+  | StitchingResolver<TResult, TParent, TContext, TArgs>;
 
 export type ResolverFn<TResult, TParent, TContext, TArgs> = (
-    parent: TParent,
-    args: TArgs,
-    context: TContext,
-    info: GraphQLResolveInfo,
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
 ) => Promise<TResult> | TResult;
 
 export type SubscriptionSubscribeFn<TResult, TParent, TContext, TArgs> = (
-    parent: TParent,
-    args: TArgs,
-    context: TContext,
-    info: GraphQLResolveInfo,
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
 ) => AsyncIterator<TResult> | Promise<AsyncIterator<TResult>>;
 
 export type SubscriptionResolveFn<TResult, TParent, TContext, TArgs> = (
-    parent: TParent,
-    args: TArgs,
-    context: TContext,
-    info: GraphQLResolveInfo,
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
 ) => TResult | Promise<TResult>;
 
-export interface SubscriptionSubscriberObject<
-    TResult,
-    TKey extends string,
-    TParent,
-    TContext,
-    TArgs,
-> {
-    subscribe: SubscriptionSubscribeFn<
-        { [key in TKey]: TResult },
-        TParent,
-        TContext,
-        TArgs
-    >;
-    resolve?: SubscriptionResolveFn<
-        TResult,
-        { [key in TKey]: TResult },
-        TContext,
-        TArgs
-    >;
+export interface SubscriptionSubscriberObject<TResult, TKey extends string, TParent, TContext, TArgs> {
+  subscribe: SubscriptionSubscribeFn<{ [key in TKey]: TResult }, TParent, TContext, TArgs>;
+  resolve?: SubscriptionResolveFn<TResult, { [key in TKey]: TResult }, TContext, TArgs>;
 }
 
 export interface SubscriptionResolverObject<TResult, TParent, TContext, TArgs> {
-    subscribe: SubscriptionSubscribeFn<any, TParent, TContext, TArgs>;
-    resolve: SubscriptionResolveFn<TResult, any, TContext, TArgs>;
+  subscribe: SubscriptionSubscribeFn<any, TParent, TContext, TArgs>;
+  resolve: SubscriptionResolveFn<TResult, any, TContext, TArgs>;
 }
 
-export type SubscriptionObject<
-    TResult,
-    TKey extends string,
-    TParent,
-    TContext,
-    TArgs,
-> =
-    | SubscriptionSubscriberObject<TResult, TKey, TParent, TContext, TArgs>
-    | SubscriptionResolverObject<TResult, TParent, TContext, TArgs>;
+export type SubscriptionObject<TResult, TKey extends string, TParent, TContext, TArgs> =
+  | SubscriptionSubscriberObject<TResult, TKey, TParent, TContext, TArgs>
+  | SubscriptionResolverObject<TResult, TParent, TContext, TArgs>;
 
-export type SubscriptionResolver<
-    TResult,
-    TKey extends string,
-    TParent = {},
-    TContext = {},
-    TArgs = {},
-> =
-    | ((
-          ...args: any[]
-      ) => SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>)
-    | SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>;
+export type SubscriptionResolver<TResult, TKey extends string, TParent = {}, TContext = {}, TArgs = {}> =
+  | ((...args: any[]) => SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>)
+  | SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>;
 
 export type TypeResolveFn<TTypes, TParent = {}, TContext = {}> = (
-    parent: TParent,
-    context: TContext,
-    info: GraphQLResolveInfo,
+  parent: TParent,
+  context: TContext,
+  info: GraphQLResolveInfo
 ) => Maybe<TTypes> | Promise<Maybe<TTypes>>;
 
-export type IsTypeOfResolverFn<T = {}, TContext = {}> = (
-    obj: T,
-    context: TContext,
-    info: GraphQLResolveInfo,
-) => boolean | Promise<boolean>;
+export type IsTypeOfResolverFn<T = {}, TContext = {}> = (obj: T, context: TContext, info: GraphQLResolveInfo) => boolean | Promise<boolean>;
 
 export type NextResolverFn<T> = () => Promise<T>;
 
-export type DirectiveResolverFn<
-    TResult = {},
-    TParent = {},
-    TContext = {},
-    TArgs = {},
-> = (
-    next: NextResolverFn<TResult>,
-    parent: TParent,
-    args: TArgs,
-    context: TContext,
-    info: GraphQLResolveInfo,
+export type DirectiveResolverFn<TResult = {}, TParent = {}, TContext = {}, TArgs = {}> = (
+  next: NextResolverFn<TResult>,
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
 ) => TResult | Promise<TResult>;
 
 /** Mapping between all available schema types and the resolvers types */
 export type ResolversTypes = ResolversObject<{
-    Mutation: ResolverTypeWrapper<{}>;
-    Boolean: ResolverTypeWrapper<Scalars['Boolean']>;
-    String: ResolverTypeWrapper<Scalars['String']>;
-    Query: ResolverTypeWrapper<{}>;
-    Int: ResolverTypeWrapper<Scalars['Int']>;
+  Mutation: ResolverTypeWrapper<{}>;
+  Photo: ResolverTypeWrapper<Photo>;
+  ID: ResolverTypeWrapper<Scalars['ID']>;
+  String: ResolverTypeWrapper<Scalars['String']>;
+  PhotoCategory: PhotoCategory;
+  PostPhotoInput: PostPhotoInput;
+  Query: ResolverTypeWrapper<{}>;
+  Int: ResolverTypeWrapper<Scalars['Int']>;
+  Boolean: ResolverTypeWrapper<Scalars['Boolean']>;
 }>;
 
 /** Mapping between all available schema types and the resolvers parents */
 export type ResolversParentTypes = ResolversObject<{
-    Mutation: {};
-    Boolean: Scalars['Boolean'];
-    String: Scalars['String'];
-    Query: {};
-    Int: Scalars['Int'];
+  Mutation: {};
+  Photo: Photo;
+  ID: Scalars['ID'];
+  String: Scalars['String'];
+  PostPhotoInput: PostPhotoInput;
+  Query: {};
+  Int: Scalars['Int'];
+  Boolean: Scalars['Boolean'];
 }>;
 
-export type MutationResolvers<
-    ContextType = any,
-    ParentType extends ResolversParentTypes['Mutation'] = ResolversParentTypes['Mutation'],
-> = ResolversObject<{
-    postPhoto?: Resolver<
-        ResolversTypes['Boolean'],
-        ParentType,
-        ContextType,
-        RequireFields<MutationPostPhotoArgs, 'name'>
-    >;
+export type MutationResolvers<ContextType = any, ParentType extends ResolversParentTypes['Mutation'] = ResolversParentTypes['Mutation']> = ResolversObject<{
+  postPhoto?: Resolver<ResolversTypes['Photo'], ParentType, ContextType, RequireFields<MutationPostPhotoArgs, 'input'>>;
 }>;
 
-export type QueryResolvers<
-    ContextType = any,
-    ParentType extends ResolversParentTypes['Query'] = ResolversParentTypes['Query'],
-> = ResolversObject<{
-    totalPhotos?: Resolver<
-        Maybe<ResolversTypes['Int']>,
-        ParentType,
-        ContextType
-    >;
+export type PhotoResolvers<ContextType = any, ParentType extends ResolversParentTypes['Photo'] = ResolversParentTypes['Photo']> = ResolversObject<{
+  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
+  url?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  name?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  description?: Resolver<Maybe<ResolversTypes['String']>, ParentType, ContextType>;
+  category?: Resolver<Maybe<ResolversTypes['PhotoCategory']>, ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+}>;
+
+export type QueryResolvers<ContextType = any, ParentType extends ResolversParentTypes['Query'] = ResolversParentTypes['Query']> = ResolversObject<{
+  totalPhotos?: Resolver<Maybe<ResolversTypes['Int']>, ParentType, ContextType>;
+  allPhotos?: Resolver<Array<ResolversTypes['Photo']>, ParentType, ContextType>;
 }>;
 
 export type Resolvers<ContextType = any> = ResolversObject<{
-    Mutation?: MutationResolvers<ContextType>;
-    Query?: QueryResolvers<ContextType>;
+  Mutation?: MutationResolvers<ContextType>;
+  Photo?: PhotoResolvers<ContextType>;
+  Query?: QueryResolvers<ContextType>;
 }>;
+
 
 /**
  * @deprecated
